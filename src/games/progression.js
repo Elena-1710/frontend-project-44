efea47ce0d382511abba18599e70068afdaefbf9
Elena-1710 <zhunina.lena@yandex.ru>
2024-10-27 11:07:32 +0300

    modified progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -16,7 +16,7 @@ const playProgression = () => {
   const progressionLength = getRandomArbitrary(5, 10);
 
   const progression = getProgression(startNum, progressionStep, progressionLength);
-  const answerIndex = getRandomArbitrary(1, progressionLength);
+  const answerIndex = getRandomArbitrary(1, progression.length);
   const questionRigth = String(progression[answerIndex]);
   progression[answerIndex] = '..';
   const question = progression.join(' ');
