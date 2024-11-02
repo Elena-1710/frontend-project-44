10403063d571fd3ff09b05c26800b24535e1b8c9
Elena-1710 <zhunina.lena@yandex.ru>
2024-11-03 00:02:21 +0300

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import readlineSync from 'readline-sync';
 
 const play = (generateRound, description) => {
-  // Приветствие
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  // выводим инструкцию
   console.log(description);
   const roundsCount = 3;
-  // Задаем 3 шага
   for (let i = 1; i <= roundsCount; i += 1) {
     const { question, correntAnswer } = generateRound();
     console.log(`Question: ${question}`);
-    // ответ пользователя
     const answer = readlineSync.question('Your answer: ');
     if (correntAnswer === answer) {
       console.log('Correct!');
